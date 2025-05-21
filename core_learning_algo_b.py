59bba080eef6847dc9262f676472ceb0e41f1271
dhelms33 <derrickhelms10@gmail.com>
2025-05-21 17:16:28 -0600

    Finished core learning working with data b

--- core_learning_algo_b.py
+++ core_learning_algo_b.py
@@ -10,12 +10,38 @@ dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv') #loads the testing dataset into a DataFrame, used to evaluate model after training
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
+#takes y_train and store it there
 
 #use dftrain.head() to show first 5 items in DataFrame
 dftrain.head()
+print(dftrain.head())
 
 #for a more statisitical analysis of or data, we use .describe()
-dftrain.describe()
+dftrain.describe() #also returns 
+
+#data is a binary representation of whether someone survide or not
+#locating row
+print(dftrain[0], y_trtrain.loc[0])
+#locating row
+
+print(dftrain[0], y_trtrain.loc[0])
+
+#get age values
+print(dftrain["age"]) #age column
+
 
 #we also should know the shape of the data as well
 dftrain.shape
+#627, 9 features
+
+#get histogram of age
+dftrain.age.hist(bins=20)
+
+#print sex calues
+print(dftrain.sex.value_counts().plot(kind = 'barh'))
+
+#print class values
+print(dftrain['class'].value_counts().plot(kind='barh'))
+
+#print survival rate
+print(pd.concat([dftrain, y_train], axis =1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive'))
